b784a2e5527737180e68eb2d3ae0a098ab91c5fc
WoodenPlancks <fishkin.michal@gmail.com>
2021-12-14 13:16:02 -0500

    Finished Day one part two. The code is now generalized to accept windows of general sizes. The answer is 1797. TODO: update it to use the commandline for input for the input filepath and the window size.

--- Day_1/D1_Code.c
+++ Day_1/D1_Code.c
@@ -3,11 +3,15 @@
 int main(void)
 {
     FILE* fp;
-    int prev = 0;
-    int curr = 0;
-    int incrCount = -1;
 
-    char filePath[] = "./D1_Input/D1_input.txt";
+    int const windowSize = 3;
+    char const filePath[] = "./D1_Input/D1_input.txt";
+
+    int window[windowSize+1];
+
+    int leadingNumInd = 0;
+    int currNum=0;
+    int incrCount = 0;
 
     fp = fopen(filePath, "r");
 
@@ -17,19 +21,44 @@ int main(void)
         return -1;
     }
 
-    while(curr != -1)
+    for(int i=0; i<windowSize+1; i++)
     {
-        fscanf(fp, "%d", &curr);
-        if(curr > prev)
+        fscanf(fp, "%d", &currNum);
+
+        window[i] = currNum;
+
+        if(currNum == -1)
         {
-            incrCount++;
+            printf("Reached the end of the file before the initial window initialized. Make sure your input has more than %d entries.\n", windowSize+1);
+            return 0;
         }
-        printf("%d, %d\n", prev, curr);
-        prev = curr;
+    }
+
+    while(1)
+    {
+        
+        fscanf(fp, "%d", &currNum);
+
+        int leadingNum = window[leadingNumInd];
+        int closingNum = window[(leadingNumInd+windowSize)%(windowSize+1)];
+
+        if(closingNum > leadingNum)
+        {
+            incrCount ++;
+        }
+
+        window[leadingNumInd] = currNum;
+        leadingNumInd = (leadingNumInd+1)%(windowSize+1);
+
+        if(currNum == -1) 
+        {
+            printf("Reached the end of the file.\n");
+            fclose(fp);
+            printf("The depth increases %d times with a window of size %d.\n", incrCount, windowSize);
+            return 0;
+        }
     }
     
-    fclose(fp);
 
-    printf("The depth increases %d times.", incrCount);
     return 0;
 }
