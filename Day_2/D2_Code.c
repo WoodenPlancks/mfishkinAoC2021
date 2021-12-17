d2ba272dd532a6b0f0099dca2533fe3393117592
WoodenPlancks <fishkin.michal@gmail.com>
2021-12-16 20:09:17 -0500

    Finished day 2 part 1: the answer was 1648020.

---
+++ Day_2/D2_Code.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int returnIndexOfFirstSpace(char []);
+
+int main(void)
+{
+    char const filePath[] = "./D2_Input/D2_input.txt";
+    char inputLine[255];
+
+    FILE* fp = fopen(filePath, "r");
+
+    if(fp == NULL)
+    {
+        printf("Could not find input text file at location %s.", filePath);
+        return -1;
+    }
+
+    int depth = 0;
+    int hPos = 0;
+
+    while (fgets(inputLine, sizeof(inputLine), fp)) 
+    {
+        int num = strtol(inputLine + getIndexOfFirstSpace(inputLine) + 1, NULL, 10);
+
+        switch(inputLine[0])
+        {
+            case 'u':
+                printf("Going up num: %d.\n", num);
+                depth -= num;
+                break;
+            case 'd':
+                printf("Going down num: %d.\n", num);
+                depth += num;
+                break;
+            default:
+                printf("Going forward num: %d.\n", num);
+                hPos += num;
+                break;
+        }
+    }
+
+    int prod = depth * hPos;
+
+    printf("\nOutput:\n");
+    printf("The final depth is %d. The final horizontal position is %d. \nTheir product is %d.", depth, hPos, prod);
+
+    fclose(fp);
+
+    return 0;
+}
+
+int getIndexOfFirstSpace(char* str)
+{
+    int currInd = 0;
+
+    while(str[currInd] != '\0')
+    {
+        if(str[currInd] == ' ')
+        {
+            return currInd;
+        }
+        currInd++;
+    }
+
+    return -1;
+}
